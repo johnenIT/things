4f62ddb8d22a5fa36964a2837c7d2ce776cd6bf5
johnenIT <141713399+johnenIT@users.noreply.github.com>
2023-10-09 01:36:13 +0200

    Update de.ps1

--- ServerCore/de.ps1
+++ ServerCore/de.ps1
@@ -1,4 +1,2 @@
 Set-WinUILanguageOverride -Language de-DE
 Set-ItemProperty -Path HKCU:\Keyboard Layout\Preload -Name 1 -Value 00000407
-
-Remove-NetIPAddress -InterfaceAlias Ethernet0 -confirm:$False
